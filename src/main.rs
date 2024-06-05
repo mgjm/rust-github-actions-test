86c7a88c882feec3abdb002162d3a8db863d028f
Martin Michaelis <code@mgjm.de>
2024-06-05 16:14:00 +0000

    Add CI config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![warn(clippy::all, clippy::pedantic)]
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "Hello, world!: {:?} {:?}",
+        std::path::MAIN_SEPARATOR,
+        std::env::current_dir().unwrap(),
+    );
+
+    foo();
+
+    match hidapi::HidApi::new() {
+        Ok(hidapi) => {
+            println!("devices:");
+            for device in hidapi.device_list() {
+                println!(
+                    "- vendor={:?} {:?} product={:?} {:?} serial={:?} path={:?}",
+                    device.vendor_id(),
+                    device.manufacturer_string(),
+                    device.product_id(),
+                    device.product_string(),
+                    device.serial_number(),
+                    device.path(),
+                );
+            }
+            println!("--- done");
+        }
+        Err(err) => eprintln!("Error: {err:?}"),
+    }
+}
+
+#[cfg(target_feature = "crt-static")]
+fn foo() {
+    println!("the C runtime should be statically linked");
+}
+
+#[cfg(not(target_feature = "crt-static"))]
+fn foo() {
+    println!("the C runtime should be dynamically linked");
 }
